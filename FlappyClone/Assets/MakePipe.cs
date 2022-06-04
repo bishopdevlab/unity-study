df83282766e54a3ee6ae38d9d88805b1939f6d11
Yong-il Lee <dbjavapia@gmail.com>
2022-06-05 02:23:43 +0900

    파이프 통과 시 점수 올리기
    10. Android 스마트폰에서 게임 실행하기 ~ 18. 파이프 통과 시 점수 올려주기

---
+++ FlappyClone/Assets/MakePipe.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Prefab 생성 속도 조절
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);  // 생성
+            // 간격 랜덤하게 조절
+            newpipe.transform.position = new Vector3(6, Random.Range(-6f, 1.7f), 0);
+            timer = 0;
+            Destroy(newpipe, 10.0f); // 메모리 관리 - 생성된 후 10초 삭제됨
+        }
+    }
+}
